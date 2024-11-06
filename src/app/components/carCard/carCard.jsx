e394c6d3e38f5b786f8cc9b5af31df067cf75905
TamariIv <taivgi@g.jct.ac.il>
2024-11-06 12:15:16 +0200

    add edit button

--- src/app/components/carCard/carCard.jsx
+++ src/app/components/carCard/carCard.jsx
@@ -1,8 +1,11 @@
 "use client"
 import React from "react";
 import styles from "./carCard.module.css";
+import { FaEdit } from "react-icons/fa";
+
 
 export default function CarCard({ car }) {
+
 
     return (
         /* From Uiverse.io by alexruix */
@@ -11,8 +14,8 @@ export default function CarCard({ car }) {
                 <p className={styles.carModel}>{car.model}</p>
                 <p className={styles.carDetail}>{car.color}</p>
                 <p className={styles.carDetail}>{car.plate}</p>
-
-            </div>
+                <FaEdit className={styles.editIcon}/>
+                </div>
             <button className={styles.cardButton}>Delete</button>
         </div>
     )
